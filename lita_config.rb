46de6f2fdcb4485b0b5ab853b73ac4d4125bf388
Martin Fenner <martin.fenner@datacite.org>
2016-08-14 09:12:20 +0200

    added additional plugins

--- lita_config.rb
+++ lita_config.rb
@@ -11,6 +11,23 @@ Lita.configure do |config|
   config.handlers.google_images.google_cse_id = ENV["GOOGLE_CSE_ID"]
   config.handlers.google_images.google_cse_key = ENV["GOOGLE_KEY"]
 
+  # librato
+  config.handlers.libratoo.email = ENV["LIBRATOO_EMAIL"]
+  config.handlers.libratoo.api_key = ENV["LIBRATOO_API_KEY"]
+
+  # pivotal tracker
+  config.handlers.pivotal_tracker.token = ENV["PIVOTAL_TOKEN"]
+
+  # statuspage
+  config.handlers.statuspage.api_key = ENV["STATUSPAGE_API_KEY"]
+  config.handlers.statuspage.page_id = ENV["STATUSPAGE_PAGE_ID"]
+
+  #timezone
+  config.handlers.timezone.default_zone = ENV["TIMEZONE_DEFAULT"]
+
+  #urban dictionary
+  config.handlers.urban_dictionary.max_response_size = 5
+
   config.redis[:url] = ENV["REDIS_URL"]
   config.robot.log_level = ENV["LOG_LEVEL"]
 end
